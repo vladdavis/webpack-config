e510648d2c1e38e97c40a14d971f33ea3bd450a4
Vlad Davis <vladdavisw@gmail.com>
2023-10-06 23:46:39 +0300

    add react ts base flow

---
+++ react-ts-base-flow/webpack.config.dev.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const workdir = process.cwd();
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: path.resolve(workdir, 'src/index.tsx'),
+    output: {
+        path: path.resolve(workdir, 'dist'),
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: {
+            index: '/',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ title: '' }),
+    ],
+};
+
+export default config;
